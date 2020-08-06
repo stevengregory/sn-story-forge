286a689d951c4511bf83ffb7a9867897f4a7d4e6
Steven Gregory <sgregory@gmail.com>
2020-08-06 18:59:54 -0500

    Removed redundant begin & end

--- src/forge.rb
+++ src/forge.rb
@@ -14,24 +14,23 @@ module StoryForge
     end
 
     def self.do_request(host, user_name, password)
-      begin
-        config = YAML.load_file('src/config.yml')
-        auth = "Basic #{Base64.strict_encode64("#{user_name}:#{password}")}"
-        url = "#{host}/api/now/table/#{config['table']}"
-        response = RestClient.get url, params: config['filter'], authorization: auth
-        data = JSON.parse(response.body)
+      config = YAML.load_file('src/config.yml')
+      auth = "Basic #{Base64.strict_encode64("#{user_name}:#{password}")}"
+      url = "#{host}/api/now/table/#{config['table']}"
+      response = RestClient.get url, params: config['filter'], authorization: auth
+      data = JSON.parse(response.body)
 
-        remove_files config['path'] if File.directory?(config['path'])
+      remove_files config['path'] if File.directory?(config['path'])
 
-        data['result'].first(config['limit']).map do |item|
-          dir_path = item['state'].to_s.downcase
-          FileUtils.mkdir_p(config['path']) if !File.directory?(config['path'])
-          Dir.mkdir config['path'] + dir_path if !File.directory?(config['path'] + dir_path)
-          file_path = "#{config['path']}#{dir_path}/#{item['number']}.md"
-          File.write(file_path, Template.get_markdown_template(item))
+      data['result'].first(config['limit']).map do |item|
+        dir_path = item['state'].to_s.downcase
+        FileUtils.mkdir_p(config['path']) if !File.directory?(config['path'])
+        Dir.mkdir config['path'] + dir_path if !File.directory?(config['path'] + dir_path)
+        file_path = "#{config['path']}#{dir_path}/#{item['number']}.md"
+        File.write(file_path, Template.get_markdown_template(item))
+      end
       rescue RestClient::ExceptionWithResponse => e
         e.response
-      end
     end
   end
 end
