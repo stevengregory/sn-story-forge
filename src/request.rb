38cdf9d9ef71592086ea36804cc5eaaf209c87af
Steven Gregory <sgregory@gmail.com>
2020-08-16 11:06:30 -0500

    Updated api url to use instance

--- src/request.rb
+++ src/request.rb
@@ -8,14 +8,14 @@ require 'rest-client'
 module StoryForge
   class Request
     def initialize
-      @host = ENV['HOST']
+      @instance = ENV['INSTANCE']
       @user_name = ENV['USERNAME']
       @password = ENV['PASSWORD']
     end
 
     def do_request(table, filter)
       auth = "Basic #{Base64.strict_encode64("#{@user_name}:#{@password}")}"
-      url = "#{@host}/api/now/table/#{table}"
+      url = "https://#{@instance}.service-now.com/api/now/table/#{table}"
       response = RestClient.get url, params: filter, authorization: auth
       JSON.parse(response.body)
     end
