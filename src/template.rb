239cd86c05caf1530bf48231a9545fdebf4cfdc2
Steven Gregory <sgregory@gmail.com>
2020-10-21 20:00:58 -0500

    Hiding empty acceptance criteria

--- src/template.rb
+++ src/template.rb
@@ -31,7 +31,7 @@ module StoryForge
     end
 
     def get_acceptance_criteria(item)
-      "## ✅ Acceptance Criteria\n\n#{Utils.new.convert_to_markdown item, 'acceptance_criteria'}"
+      "## ✅ Acceptance Criteria\n\n#{Utils.new.convert_to_markdown item, 'acceptance_criteria'}" if !item['acceptance_criteria'].empty?
     end
 
     def get_assigned_to(item)
